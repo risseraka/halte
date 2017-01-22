02481a8b490cc3ad2856e321c4c3672919d5e767
Adrien Risser <risseraka@gmail.com>
2017-01-22 15:08:28 +0100

    using  instead of

--- index.js
+++ index.js
@@ -1,32 +1,64 @@
 const fs = require('fs');
 const path = require('path');
+const vm = require('vm');
 
-exports = module.exports = (app, { dir }) => {
+exports = module.exports = (app, options = {}) => {
+  const { dir, cache = true, setContentType = true, locals = {} } = options;
+
+  const internalCache = {};
+
   app.use((req, res, next) => {
+    function render(file, data) {
+      if (!data) return obj => render(file, obj);
+
+      if (setContentType) {
+        res.set('Content-Type', 'application/hal+json');
+      }
+
+      const filePath = path.join(dir, `${file.replace(/\.js$/, '')}.js`);
+
+      const content = cache && internalCache[filePath] ?
+              internalCache[filePath] :
+              (internalCache[filePath] = fs.readFileSync(filePath));
+
+      const context = Object.assign(
+        {
+          absolute(path) {
+            return `${req.protocol}://${req.get('host')}${req.baseUrl}${path}`;
+          },
+          get(path) {
+            try {
+              with (data) {
+                return eval(path);
+              }
+            } catch (e) {
+            }
+            return undefined;
+          },
+          render,
+        },
+        locals,
+        { data, result: null }
+      );
+
+      const script = new vm.Script(
+        `with (data) { result = ${content.toString()} }`,
+        { filename: filePath, displayErrors: true }
+      );
+
+      script.runInContext(new vm.createContext(context));
+
+      return context.result;
+    };
+
     res.hal = (file, data) => {
-      res.set('Content-Type', 'application/hal+json');
+      try {
+        const result = render(file, data);
 
-      const filePath = path.join(dir, `${file}.js`);
-
-      fs.readFile(filePath, (err, content) => {
-        if (err) return next(err);
-
-        const { baseUri } = req;
-
-        function absolute(path) {
-          return `${req.protocol}://${req.get('host')}${req.baseUrl}${path}`;
-        }
-
-        try {
-          with (data) {
-            return res.json(eval(`(${content.toString()})`));
-          }
-        } catch (e) {
-          console.error(`Template error in file "${file}"`);
-
-          return next(new Error(`Template error: ${e}`));
-        }
-      });
+        res.json(result);
+      } catch (e) {
+        next(e);
+      }
     };
 
     next();
